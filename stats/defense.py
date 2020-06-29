9758e70e0fecf03d56f7adf28db1961012916ff9
Brian Lu <45515803+bl2456@users.noreply.github.com>
2020-06-29 01:09:02 -0400

    Completed module

--- stats/defense.py
+++ stats/defense.py
@@ -6,15 +6,14 @@ from frames import games,info,events
 plays = games.query('type == "play" and event != "NP"')
 plays.columns = ['type', 'inning', 'team', 'player', 'count', 'pitches', 'event', 'game_id', 'year']
 
-pa = plays.loc[plays['player'].shift() != plays['player']]
-pa = pa[['year', 'game_id', 'inning', 'team', 'player']]
+pa = plays.loc[plays['player'].shift() != plays['player'], ['year', 'game_id', 'inning', 'team', 'player']]
 pa = pa.groupby(['year', 'game_id', 'team']).size().reset_index(name='PA')
 
 events = events.set_index(['year', 'game_id', 'team', 'event_type'])
 events = events.unstack().fillna(0).reset_index()
 events.columns = events.columns.droplevel()
 events.columns = ['year', 'game_id', 'team', 'BB', 'E', 'H', 'HBP', 'HR', 'ROE', 'SO']
-events = events.rename_axis(None)
+events = events.rename_axis(None, axis='columns')
 #print(events)
 #print(pa.columns)
 #print(events.columns)
